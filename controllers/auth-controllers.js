178ad2cb16eaaf19cd77f76011f3e2db8e816cb7
Paul Chheang <phalcheat@gmail.com>
2020-12-21 13:33:58 -0600

    Added user login with JSON Web Token for authentication mechanism

--- controllers/auth-controllers.js
+++ controllers/auth-controllers.js
@@ -1,5 +1,7 @@
 const { validationResult } = require("express-validator/check");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const JWT_SECRET = process.env.JWT_SECRET;
 
 const User = require("../models/user");
 
@@ -26,6 +28,45 @@ exports.signup = (req, res, next) => {
     })
     .then((result) => {
       res.status(201).json({ message: "User created!", userId: result._id });
+    })
+    .catch((err) => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
+    });
+};
+
+exports.login = (req, res, next) => {
+  const email = req.body.email;
+  const password = req.body.password;
+  let loadedUser;
+  User.findOne({ email: email })
+    .then((user) => {
+      if (!user) {
+        const error = new Error("A user with this email could not be found.");
+        error.statusCode = 401;
+        throw error;
+      }
+      loadedUser = user;
+      return bcrypt.compare(password, user.password);
+    })
+    .then((isEqual) => {
+      if (!isEqual) {
+        const error = new Error("Wrong password!");
+        error.statusCode = 401;
+        throw error;
+      }
+      // Create json web token and signature
+      const token = jwt.sign(
+        {
+          email: loadedUser.email,
+          userId: loadedUser._id.toString(),
+        },
+        JWT_SECRET,
+        { expiresIn: "1h" }
+      );
+      res.status(200).json({ token: token, userId: loadedUser._id.toString() });
     })
     .catch((err) => {
       if (!err.statusCode) {
--- routes/auth-routes.js
+++ routes/auth-routes.js
@@ -26,4 +26,5 @@ router.put(
   authController.signup
 );
 
+router.post("/login", authController.login);
 module.exports = router;
